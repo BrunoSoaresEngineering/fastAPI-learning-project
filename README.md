dbbacfc385a65efe35d86b8abfae6872927cc236
Bruno Soares <brunommsoares@outlook.com>
2025-01-15 20:02:07 +0000

    feat: add "product" table

--- README.md
+++ README.md
@@ -11,5 +11,21 @@ erDiagram
       integer parent_id FK
     }
 
+    Product {
+      integer id PK "NN"
+      UUID pid UK "NN"
+      string(200) name UK "NN"
+      string(220) slug UK "NN"
+      text description
+      boolean is_digital "NN DF(False)"
+      datetime created_at "NN"
+      datetime updated_at "NN"
+      boolean is_active "NN DF(False)"
+      enum stock_status "NN DV(OutOfStock)"
+      integer category_id FK "NN"
+    }
+
     Category |o--o{ Category : "has parent"
+    Product }o--|| Category: "belongs to"
+
 ```
--- app/models.py
+++ app/models.py
@@ -1,10 +1,17 @@
 from sqlalchemy import (
+    UUID,
     Boolean,
     CheckConstraint,
     Column,
+    DateTime,
+    Enum,
+    ForeignKey,
     Integer,
     String,
+    Text,
     UniqueConstraint,
+    func,
+    text,
 )
 
 from .db_connection import Base
@@ -25,4 +32,44 @@ class Category(Base):
         CheckConstraint("LENGTH(slug) > 0", name="category_slug_length_check"),
         UniqueConstraint("name", "level", name="uq_category_name_level"),
         UniqueConstraint("slug", name="uq_category_slug"),
+    )
+
+
+class Product(Base):
+    __tablename__ = "product"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    pid = Column(
+        UUID(as_uuid=True),
+        unique=True,
+        nullable=False,
+        server_default=text("uuid_generate_v4()"),
+    )
+    name = Column(String(200), unique=True, nullable=False)
+    slug = Column(String(220), unique=True, nullable=False)
+    description = Column(Text, nullable=True)
+    is_digital = Column(Boolean, nullable=False, default=False, server_default="false")
+    created_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP"),
+        onupdate=func.now(),
+    )
+    is_active = Column(Boolean, nullable=False, default=False, server_default="false")
+    stock_status = Column(
+        Enum("oos", "is", "obo", name="status_enum"),
+        nullable=False,
+        server_default="oos",
+    )
+    category_id = Column(Integer, ForeignKey("category.id"), nullable=False)
+
+    __table_args__ = (
+        CheckConstraint("LENGTH(name) > 0", name="product_name_length_check"),
+        CheckConstraint("LENGTH(slug) > 0", name="product_slug_length_check"),
+        UniqueConstraint("pid", name="uq_product_pid"),
+        UniqueConstraint("name", name="uq_product_name"),
+        UniqueConstraint("slug", name="uq_product_slug"),
     )
