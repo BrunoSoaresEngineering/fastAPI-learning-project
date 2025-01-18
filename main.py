0d79faf817563093c07a3f6b4ca98521ee57a5a6
Bruno Soares <brunommsoares@outlook.com>
2025-01-18 16:10:37 +0000

    feat: add logger configuration

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+import logging
+import logging.config
+
+from fastapi import FastAPI, HTTPException
+
+logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
-@app.get("/hello")
-def hello():
-    return {"Hello": "How are you doing?"}
+@app.get("/")
+async def read_root():
+    try:
+        1 / 0
+    except Exception as e:
+        logging.error(f"An Exception occurred: {e}")
+        raise HTTPException(status_code=500, detail="An error occurred")
